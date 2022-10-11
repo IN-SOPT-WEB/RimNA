3cd430f8d1313e3e8293bfef9d4e50634724cbec
나림 <narim@nalims-MacBook-Air.local>
2022-10-11 18:46:35 +0900

    feat: 리스트 삭제 및 추가 기능

--- week2/todoList/todo.js
+++ week2/todoList/todo.js
@@ -1,47 +1,69 @@
-function useViewState()
+function useTodoList({ container, input, submit, deleteBtn }) {
+  const onAdd = (event) => {
+    let parentContainer =
+      event.target.parentElement.parentElement.parentElement;
+    let realContainer = parentContainer.querySelector(".todo-list");
 
-
-function useTodoList({container,input,submit}){
-    const onAdd=()=>{
-        const currentValue=input.value;
-
-        const currentLi = document.createElement("li");
-        currentLi.classList.add("todo__item");
-        currentLi.innerHTML=currentValue;
-
-        const currentDeleteButton= document.createElement("button");
-        currentDeleteButton.innerHTML=`<span class="material-icons"></span>`;
-
-        // currentDeleteButton.addEventListener("click",currentLi)
-
-        currentLi.appendChild(currentDeleteButton);
-
-        container.appendChild(currentLi);
-    };
-
-    submit.addEventListner("click",onAdd);
-    // input.addEventListner("keydown",onAdd);
-
-};
-
-function useViewState({left}){
-    const test=()=>{
-        console.log('냐냐')
+    let realInput = null;
+    if (parentContainer.className == "left") {
+      realInput = input[0];
+    } else {
+      realInput = input[1];
     }
-    left.addEventListner('click',test);
-};
 
+    const currentValue = realInput.value;
 
-function main(){
-    useViewState({
-        left: document.querySelector("left"),
-    });
-    useTodoList({
-        // container: document.querySelector("todo-list")
-        // input:
-        // submit: 
-    })
+    const currentLi = document.createElement("li");
+    currentLi.classList.add("todo__item");
+    currentLi.innerHTML = currentValue;
+
+    const currentDeleteButton = document.createElement("button");
+    currentDeleteButton.classList.add("todo__item__btn-delete");
+    currentDeleteButton.innerHTML = `<span class="material-icons"></span>`;
+    currentDeleteButton.addEventListener("click", onDelete);
+
+    // currentDeleteButton.addEventListener("click",currentLi)
+    currentLi.appendChild(currentDeleteButton);
+
+    realContainer.appendChild(currentLi);
+  };
+
+  const onDelete = (event) => {
+    let parent = event.target.parentElement.parentElement;
+    parent.remove();
+  };
+
+  for (let i = 0; i < submit.length; i++) {
+    submit[i].addEventListener("click", onAdd);
+  }
+  for (let i = 0; i < deleteBtn.length; i++) {
+    deleteBtn[i].addEventListener("click", onDelete);
+  }
 }
 
+function useViewState({ left, right }) {
+  //   console.log(left);
+  //   const hidden = (e) => {
+  //     console.log("냐냐");
+  //   };
+  //   left.addEventListner("click", hidden);
+}
+
+function main() {
+  let test = document.querySelectorAll(".todo__item__btn-delete");
+  console.log(test);
+
+  useViewState({
+    left: document.querySelector(".left"),
+    right: document.querySelector(".right"),
+  });
+
+  useTodoList({
+    container: document.querySelector(".todo-list"),
+    input: document.querySelectorAll(".todo-add-group__input"),
+    submit: document.querySelectorAll(".todo-add-group__btn-add"),
+    deleteBtn: document.querySelectorAll(".todo__item__btn-delete"),
+  });
+}
 
 main();
