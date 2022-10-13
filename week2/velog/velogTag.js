9488bcfbd9b3368b99825d3fe59794d918206499
나림 <narim@nalims-MacBook-Air.local>
2022-10-13 17:19:42 +0900

    feat: 로컬스토리지에 태그 저장 기능

--- week2/velog/velogTag.js
+++ week2/velog/velogTag.js
@@ -1,3 +1,9 @@
+//로컬스토리지에서 태그 가져와서 배열 형태로 바꾸기
+let tagList = JSON.parse(localStorage.getItem("tag"));
+if (tagList === null) {
+  tagList = [];
+}
+
 const tagIsOkay = (tag) => {
   if (tag == undefined || tag == null || tag.length <= 0) {
     return false;
@@ -21,14 +27,30 @@ const addTag = (e) => {
 
     tagList.push(e.target.value);
     e.target.value = null;
+    localStorage.setItem("tag", JSON.stringify(tagList));
   }
 };
 
 const removeTag = (e) => {
   tagList = tagList.filter((element) => element != e.target.innerHTML);
+  localStorage.setItem("tag", JSON.stringify(tagList));
   e.currentTarget.remove();
 };
 
-let tagList = []; //중복된 태그를 확인하기 위해 태그 리스트 저장
-const tagInput = document.querySelector(".input-tag");
-tagInput.addEventListener("keyup", addTag);
+const main = (e) => {
+  //localStorage에서 가져온 리스트 수만큼 바로 엘리멘트 추가!
+  for (let i = 0; i < tagList.length; i++) {
+    console.log("냐냐");
+    const newTag = document.createElement("button");
+    newTag.className = "tag";
+    newTag.addEventListener("click", removeTag);
+
+    newTag.innerHTML = tagList[i];
+
+    const parentElement = document.querySelector(".tag-container");
+    parentElement.appendChild(newTag);
+  }
+  const tagInput = document.querySelector(".input-tag");
+  tagInput.addEventListener("keyup", addTag);
+};
+main();
